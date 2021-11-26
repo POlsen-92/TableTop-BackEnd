c51ad2e5431850f7ec67d24482ab23ae314c8153
mark.lohsemiranda <mark.lohsemiranda@gmail.com>
2021-11-26 14:03:20 -0800

    fixed find one user with campaign as gm

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -97,16 +97,16 @@ router.get("/", tokenAuth, async (req, res) => {
     const userData = await User.findByPk(req.user.id, {
       include: [Campaign, Character, Blog, Comment, Invite],
     });
-    // const campaignData = await Campaign.findAll({
-    //   where: {
-    //     gm_id: req.user.id
-    //   }
-    // });
+    const campaignData = await Campaign.findAll({
+      where: {
+        gm_id: req.user.id
+      }
+    });
     if (!userData) {
       res.status(404).json({ message: "No User found" });
       return;
     }
-    res.status(200).json(userData);
+    res.status(200).json([userData, campaignData]);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
